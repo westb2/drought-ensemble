e2cff2268b748be709e7cbf0430b620184691c08
westb2 <west.bd@gmail.com>
2025-09-24 20:11:48 -0700

    Add demo.py

---
+++ classes/demo.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+from RunOutputReader import RunOutputReader
+from Run import Run
+from Domain import Domain
+
+domain = Domain(config_file="/Users/ben/Documents/GitHub/drought-ensemble/domains/wolf_test/config.ini", TESTING=True)
+domain.get_domain()
+domain = Domain(config_file="/Users/ben/Documents/GitHub/drought-ensemble/domains/wolf_test/config.ini", TESTING=True)
+domain.get_domain()
+
+run = Run(sequence="simple_test.json", domain=domain)
+run.run_full_sequence()
+
+run_output_reader = RunOutputReader(run)
+data = run_output_reader.read_output(save_to_file=True)
